86483cac01581b7fddcf24639738b7ab5bbc4fce
ezra edelshtain <ezra.edl@gmail.com>
2025-12-31 10:07:02 +0200

    Remove unnecessary blank lines in __init__.py for improved code clarity.

--- homeharvest/__init__.py
+++ homeharvest/__init__.py
@@ -11,9 +11,6 @@ from .core.scrapers.realtor import RealtorScraper
 from .core.scrapers.models import ListingType, SearchPropertyType, ReturnType, Property
 from typing import Union, Optional, List
 
-
-
-
 def scrape_property(
     location: str,
     listing_type: str | list[str] | None = None,
